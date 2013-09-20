94e5f3a9c8ef76260e70a0a0d7913d857253346b
ilya.drabenia <ilya.drabenia@altoros.com>
2013-09-20 16:05:42 +0300

    Add JSDoc

--- app/scripts/controllers/dna-frequencies.js
+++ app/scripts/controllers/dna-frequencies.js
@@ -7,6 +7,9 @@
 
         .controller('DnaFrequenciesCtrl', ['$scope', 'Dna', function ($scope, Dna) {
 
+            /**
+             * View initialization function
+             */
             function init () {
                 $scope.dnaList = [];
                 $scope.newDna = new Dna();
@@ -14,11 +17,21 @@
 
             init();
 
+            /**
+             * Function move DNA sequence from form to in-memory storage, and reset form
+             */
             $scope.addDna = function () {
                 $scope.dnaList.push($scope.newDna);
                 $scope.newDna = new Dna();
             };
 
+            /**
+             * Checks when field contains error value
+             *
+             * @param {Field} field
+             * @param {string} type
+             * @returns {boolean} returns true if field is not pass validation; otherwise returns false.
+             */
             $scope.hasError = function(field, type) {
                 return field.$dirty && field.$error[type];
             };
